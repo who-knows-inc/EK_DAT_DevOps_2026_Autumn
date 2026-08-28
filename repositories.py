e4f48da98abd5554ec3bf2ada5aa28df234eaf8b
YacqubJAD <yadi0001@stud.kea.dk>
2026-08-28 15:46:26 +0200

    added group 'whoKnowsXD'

--- repositories.py
+++ repositories.py
@@ -1,4 +1,13 @@
 GROUP_REPOS = [
-    
+{
+    "name": "WhoKnowsXD",
+    "gitLinks": ["https://github.com/WeKnowXD"],
+    "backend": "",
+    "frontend": "",
+    "monitoring": "",
+    "stack": [],
+    "documentation": [],
+    "sla": "",
+}    
 ]
 
