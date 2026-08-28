af98b712fb7ced7cb6e4e0143e59b3790d18d04e
PigeonPC <olgakatinka@gmail.com>
2026-08-28 15:52:06 +0200

    Add MaxiKnows repository details
    Added a new repository entry for MaxiKnows with relevant details.

--- repositories.py
+++ repositories.py
@@ -9,5 +9,16 @@ GROUP_REPOS = [
         "documentation": [],
         "sla": "",
     }
+
+    {
+    "name": "MaxiKnows",
+    "gitLinks": ["https://github.com/maxiknows/maxi-knows.git"],
+    "backend": "",
+    "frontend": "",
+    "monitoring": "",
+    "stack": [],
+    "documentation": [],
+    "sla": "",
+    }
 ]
 
